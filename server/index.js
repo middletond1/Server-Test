e7a046cfd38a64b6c4e323a90e830224f39d17a7
David Middleton <middleton.davida@gmail.com>
2022-04-09 12:50:03 -0400

    moved files into server folder

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 app.get("/app", (req, res) => {
     res.json({ message: "Hello from server!" });
   });
